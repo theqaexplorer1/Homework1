fcc0e27c673dcaca4575a0a1b7a2c8c65abb3f38
Oleg <olegomil325@gmail.com>
2026-03-15 19:11:56 +0300

    added homework 5

---
+++ src/homework1/Homework1_5.java
@@ -0,0 +1,13 @@
+package homework1;
+
+import java.util.Arrays;
+
+public class Homework1_5 {
+    public static void main(String[] args) {
+        int q = 21;
+        int w = 8;
+        int n = q/w;
+        int n1 = q%w;
+        System.out.println(q+" / "+w+" = "+n+" и "+n1+" в остатке");
+    }
+}
