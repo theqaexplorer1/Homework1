91b82e66c21c23683813208e4559fe2559e477c9
Oleg <olegomil325@gmail.com>
2026-03-15 19:16:37 +0300

    added homework 6

---
+++ src/homework1/Homework1_6.java
@@ -0,0 +1,15 @@
+package homework1;
+
+import java.util.Arrays;
+
+public class Homework1_6 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int n = a;
+        a = b;
+        b = n;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
